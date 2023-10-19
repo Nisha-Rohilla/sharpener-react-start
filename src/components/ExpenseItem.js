fb51092a43b756acd03affe90002abe9afbb6fea
Nisha-Rohilla <rohilla.nisha15@gmail.com>
2023-10-19 17:50:38 +0530

    first-commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+const ExpenseItem = () =>{
+    const food = 10;
+    const petrol = 100;
+    const movie = 200;
+    return(
+        <div>
+            <h3>Food Rs {food}</h3>
+            <h3>Petrol Rs {petrol}</h3>
+            <h3>Movie Rs {movie}</h3>
+        </div>
+    )
+}
+
+export default ExpenseItem;
